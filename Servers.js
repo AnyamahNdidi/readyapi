0118519ec6d422c377e8eafe3c0b6ce32ce1c76a
AnyamahNdidi <theo4felix@gmail.com>
2021-05-20 23:53:15 +0100

    hello worlF

---
+++ Servers.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = 5000;
+
+
+
+const DB_ONLINE = "mongodb+srv://shotcode:ilovemusic1234@cluster0.g51is.mongodb.net/shotcode?retryWrites=true&w=majority";
+
+const app = express();
+
+
+mongoose
+  .connect(DB_ONLINE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database has been connected successfully...!");
+  });
+
+
+
+
+app.get("/", async (req, res) => {
+  res.status(200).send("API is ready for consumption");
+});
+
+app.listen(port, () => {
+  console.log(`server is ready to listen to port: ${port}`);
+});
